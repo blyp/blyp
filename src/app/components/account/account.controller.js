b6845d8dabdd06c4b7c454b4d267ea8c198e0b0e
Leonardo Moreira <eu@leonardomoreira.com.br>
2016-04-01 14:09:23 -0300

    fix dropdown menu on mobile

--- src/app/components/account/account.controller.js
+++ src/app/components/account/account.controller.js
@@ -15,5 +15,56 @@ app.controller('account.controller', [
 
     $scope.alias = $route.current.$$route.alias;
 
+    $scope.active = {
+        icon : null,
+        title : null
+    };
+
+    $scope.init = {
+
+        pushActive : function(alias) {
+
+            switch (alias) {
+
+                case 'profile' :
+                    $scope.active.icon = 'edit';
+                    $scope.active.title = 'Editar Perfil';
+                    break;
+
+                case 'password' :
+                    $scope.active.icon = 'vpn_key';
+                    $scope.active.title = 'Mudar Senha';
+                    break;
+
+                case 'coupons' :
+                    $scope.active.icon = 'local_offer';
+                    $scope.active.title = 'Cupons';
+                    break;
+
+                case 'bookmarks' :
+                    $scope.active.icon = 'bookmark_border';
+                    $scope.active.title = 'Recomendações';
+                    break;
+
+                case 'comments' :
+                    $scope.active.icon = 'comment';
+                    $scope.active.title = 'Comentários';
+                    break;
+
+                case 'favorites' :
+                    $scope.active.icon = 'favorite';
+                    $scope.active.title = 'Favoritos';
+                    break;
+
+                default :
+                    $scope.active.icon = 'account_circle';
+                    $scope.active.title = 'Visão Geral';
+                    break;
+            }
+        }
+    };
+
+    $scope.init.pushActive($scope.alias);
+
 }]);
 
