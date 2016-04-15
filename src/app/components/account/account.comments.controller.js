572b216d4c89f47bc5ef17753d65fcb5afeda993
Leonardo Moreira <eu@leonardomoreira.com.br>
2016-04-15 18:00:11 -0300

    comments list

--- src/app/components/account/account.comments.controller.js
+++ src/app/components/account/account.comments.controller.js
@@ -13,5 +13,34 @@ app.controller('account.comments.controller', [
 
     var base = angular.element("#account");
 
+    $scope.comments = [
+        {
+            "text" : "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.",
+            "date" : "16/01/2016",
+            "article" : {
+                "title" : "Mussum ipsum cacilds, vidis litro abertis.",
+                "url" : "/articles/mussum-ipsum-cacilds-vidis-vitro-abertis",
+                "image" : "https://cdn-images-1.medium.com/fit/t/800/240/1*7WrDvHvEaMnsSo7s0-W9pw.jpeg",
+            }
+        },
+        {
+            "text" : "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.",
+            "date" : "16/01/2016",
+            "article" : {
+                "title" : "Mussum ipsum cacilds, vidis litro abertis.",
+                "url" : "/articles/mussum-ipsum-cacilds-vidis-vitro-abertis",
+                "image" : "https://cdn-images-1.medium.com/fit/t/800/240/1*7WrDvHvEaMnsSo7s0-W9pw.jpeg",
+            }
+        },
+        {
+            "text" : "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.",
+            "date" : "16/01/2016",
+            "article" : {
+                "title" : "Mussum ipsum cacilds, vidis litro abertis.",
+                "url" : "/articles/mussum-ipsum-cacilds-vidis-vitro-abertis",
+                "image" : "https://cdn-images-1.medium.com/fit/t/800/240/1*7WrDvHvEaMnsSo7s0-W9pw.jpeg",
+            }
+        }
+    ];
 }]);
 
